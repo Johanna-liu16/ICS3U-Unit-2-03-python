e45bdd88fc20fd8971a9039f6c8f5da51cbf5640
Johanna Liu <johanna.liu@mths.ca>
2022-09-28 15:09:03 +0000

    init

---
+++ circumference.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Sep 2022
+# This program calculates the circumference
+
+import constants
+
+
+def main():
+    # this function calculates the circumference
+
+    # input
+    print("Circumference = τr")
+    radius = int(input("Enter radius of a circle: "))
+
+    # process
+    circumference = constants.TAU * radius
+
+    # output
+    print("The circumference is {0} mm².".format(circumference))
+    
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Sep 2022
+# This file holds the constants for the circumference program
+
+TAU = 6.28318530718
